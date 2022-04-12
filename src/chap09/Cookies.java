2bb1ad9aefbe1ab601dd7d39b111f7a77c4bbf14
sebaek <sebaek@gmail.com>
2022-04-12 11:12:02 +0900

    cookies

--- src/chap09/Cookies.java
+++ src/chap09/Cookies.java
@@ -1,6 +1,33 @@
 package chap09;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 // 책 222 
 public class Cookies {
+	private Map<String, Cookie> cookieMap = new HashMap<>();
+	
+	public Cookies(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		
+		if (cookies != null) {
+			for (int i = 0; i < cookies.length; i++) {
+				cookieMap.put(cookies[i].getName(), cookies[i]);
+			}
+		}
+	}
+	
+	public boolean exists(String name) {
+		return cookieMap.get(name) != null;
+	}
+	
 
+	public static Cookie createCookie(String name, String value) throws IOException {
+		return new Cookie(name, URLEncoder.encode(value, "utf-8"));
+	}
 }
