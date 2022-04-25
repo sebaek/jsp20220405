fd47fd44c118e5f26925df0f4747ac9632152265
sebaek <sebaek@gmail.com>
2022-04-25 11:41:28 +0900

    city list, country list

--- src/chap14/S14Servlet14.java
+++ src/chap14/S14Servlet14.java
@@ -1,11 +1,19 @@
 package chap14;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class S14Servlet14
@@ -27,6 +35,41 @@ public class S14Servlet14 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		List<String> list1 = new ArrayList<>();
+		List<String> list2 = new ArrayList<>();
+
+		String sql1 = "SELECT DISTINCT Country FROM Customers ORDER BY Country";
+		String sql2 = "SELECT DISTINCT City FROM Customers ORDER BY City";
+
+		ServletContext application = getServletContext();
+		DataSource ds = (DataSource) application.getAttribute("dbpool");
+
+		try (Connection con = ds.getConnection();
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql1);) {
+
+			while (rs.next()) {
+				list1.add(rs.getString(1));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try (Connection con = ds.getConnection();
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql2);) {
+
+			while (rs.next()) {
+				list2.add(rs.getString(1));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		request.setAttribute("countryList", list1);
+		request.setAttribute("cityList", list2);
 		
 		String path = "/WEB-INF/view/chap14/ex08.jsp";
 		request.getRequestDispatcher(path).forward(request, response);
