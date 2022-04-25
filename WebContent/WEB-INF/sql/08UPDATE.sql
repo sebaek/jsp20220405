5de7710f18a677e7dfcdc9dd9bcc64a762340680
sebaek <sebaek@gmail.com>
2022-04-25 16:01:27 +0900

    update

--- WebContent/WEB-INF/sql/08UPDATE.sql
+++ WebContent/WEB-INF/sql/08UPDATE.sql
@@ -6,3 +6,8 @@ UPDATE Employees
 SET LastName = 'Sunja',
     FirstName = 'YuhJung'
 WHERE EmployeeID = 1;
+
+-- 예제1) 1번 고객(Customers Table)의 
+-- 계약자명(ContactName)은 'Alfred Schmidt'
+-- 도시(City)는 'Frankfurt'로 변경
+
