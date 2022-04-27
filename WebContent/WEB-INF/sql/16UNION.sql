ea52d7f06173c493e1c96b2b672b798593e18e14
sebaek <sebaek@gmail.com>
2022-04-27 12:09:05 +0900

    union

--- WebContent/WEB-INF/sql/16UNION.sql
+++ WebContent/WEB-INF/sql/16UNION.sql
@@ -41,3 +41,28 @@ UNION
 SELECT City FROM Customers;
 
 
+SELECT LastName, FirstName, 'samsung' AS company FROM Employees;
+
+
+-- leetcode 1795
+# Write your MySQL query statement below
+
+SELECT product_id, 'store1' store, store1 price
+FROM Products
+WHERE store1 IS NOT NULL
+
+UNION
+
+SELECT product_id, 'store2' store, store2 price
+FROM Products
+WHERE store2 IS NOT NULL
+
+UNION
+
+SELECT product_id, 'store3' store, store3 price
+FROM Products
+WHERE store3 IS NOT NULL;
+
+
+
+
