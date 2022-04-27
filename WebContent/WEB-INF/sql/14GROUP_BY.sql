e818d7d461595b567406aee80cfcee89b685b416
sebaek <sebaek@gmail.com>
2022-04-27 11:27:45 +0900

    group by

--- WebContent/WEB-INF/sql/14GROUP_BY.sql
+++ WebContent/WEB-INF/sql/14GROUP_BY.sql
@@ -44,5 +44,12 @@ GROUP BY OrderDate, EmployeeID
 ORDER BY OrderDate, EmployeeID;
 
 
-
+-- leetcode 1693
+# Write your MySQL query statement below
+SELECT date_id, 
+       make_name, 
+       COUNT(DISTINCT lead_id) AS unique_leads, 
+       COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id, make_name;
 
