f3dd4bed91b5aa76ae27b8e4b647945a25d0e1fb
sebaek <sebaek@gmail.com>
2022-04-28 15:21:19 +0900

    list

--- src/app01/dao/BoardDao.java
+++ src/app01/dao/BoardDao.java
@@ -2,8 +2,12 @@ package app01.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import app01.dto.BoardDto;
 
@@ -34,7 +38,43 @@ public class BoardDao {
 		// 결과 return
 		return result == 1;
 	}
+
+	public List<BoardDto> list(Connection con) {
+		
+		List<BoardDto> list = new ArrayList<>();
+		
+		String sql = "SELECT id, title, inserted FROM Board ORDER BY id DESC";
+
+		try (Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);) {
+			
+			while (rs.next()) {
+				BoardDto board = new BoardDto();
+				board.setId(rs.getInt(1));
+				board.setTitle(rs.getString(2));
+				board.setInserted(rs.getTimestamp(3).toLocalDateTime());
+				
+				list.add(board);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/app01/dto/BoardDto.java
+++ src/app01/dto/BoardDto.java
@@ -1,8 +1,28 @@
 package app01.dto;
 
+import java.time.LocalDateTime;
+
 public class BoardDto {
+	private int id;
 	private String title;
 	private String body;
+	private LocalDateTime inserted;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getInserted() {
+		return inserted;
+	}
+
+	public void setInserted(LocalDateTime inserted) {
+		this.inserted = inserted;
+	}
 
 	public String getTitle() {
 		return title;
