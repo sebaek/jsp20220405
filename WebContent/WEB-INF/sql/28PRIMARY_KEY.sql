1bdb5a1539558623e2c5aeab5769c9c98e583a55
sebaek <sebaek@gmail.com>
2022-04-28 12:26:11 +0900

    auto increment

--- WebContent/WEB-INF/sql/28PRIMARY_KEY.sql
+++ WebContent/WEB-INF/sql/28PRIMARY_KEY.sql
@@ -20,4 +20,37 @@ INSERT INTO mytable13 (col1, col2) VALUES ('a', null);
 SELECT * FROM mytable13;
 
 
+-- auto_increment : 자동으로 값 증가 
+CREATE TABLE mytable18 (
+	col1 INT PRIMARY KEY AUTO_INCREMENT, -- 1부터 자동으로 1씩 증가하는 값 입력해줌
+    col2 VARCHAR(10)
+);
+select * from mytable18;
+INSERT INTO mytable18 (col2) VALUES ('abce');
+
+DELETE FROM mytable18 WHERE col1 = 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
