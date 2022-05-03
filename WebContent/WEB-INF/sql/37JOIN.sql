38e9659b79500aceab064e154a3bd60979f9161f
sebaek <sebaek@gmail.com>
2022-05-03 11:29:53 +0900

    OUTER JOIN

--- WebContent/WEB-INF/sql/37JOIN.sql
+++ WebContent/WEB-INF/sql/37JOIN.sql
@@ -3,3 +3,8 @@ USE w3schools;
 -- Orders : 주문
 -- Customers : 고객
 -- 주문한 적 없는 고객 명 조회
+SELECT c.CustomerName
+FROM Customers c LEFT JOIN Orders o ON c.CustomerID = o.CustomerID
+WHERE o.OrderID IS NULL
+ORDER BY 1
+; 
