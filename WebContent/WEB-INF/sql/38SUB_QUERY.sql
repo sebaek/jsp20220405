7ccc6b91c5625bfbd1dbe4e9ce784805745391be
sebaek <sebaek@gmail.com>
2022-05-03 14:27:15 +0900

    subquery

--- WebContent/WEB-INF/sql/38SUB_QUERY.sql
+++ WebContent/WEB-INF/sql/38SUB_QUERY.sql
@@ -22,7 +22,17 @@ FROM Board
 WHERE id NOT IN  -- (13, 15, 20, 21);
 				(SELECT board_id FROM Reply);
                 
-                
+
+-- 게시글별 댓글 수 (JOIN)
+SELECT b.*, COUNT(r.id) NumOfReply
+FROM Board b LEFT JOIN Reply r ON b.id = r.board_id
+GROUP BY b.id
+ORDER BY b.id DESC;
+
+-- 게시글별 댓글 수 (SUBQUERY)
+SELECT b.*, (SELECT COUNT(r.id) FROM Reply r WHERE board_id = b.id) NumOfReply
+FROM Board b
+ORDER BY b.id DESC; 
 
 
 
--- WebContent/WEB-INF/sql/39SUB_QUERY.sql
+++ WebContent/WEB-INF/sql/39SUB_QUERY.sql
@@ -2,3 +2,15 @@ USE w3schools;
 
 -- 주문한 적 없는 고객명 조회(JOIN, SUB QUERY)
 -- Orders, Customers
+-- JOIN
+SELECT c.CustomerName
+FROM Customers c LEFT JOIN Orders o
+ON c.CustomerID = o.CustomerID
+WHERE o.OrderID IS NULL
+ORDER BY 1;
+
+-- SUB QUERY
+SELECT CustomerName
+FROM Customers
+WHERE CustomerID NOT IN (SELECT CustomerID FROM Orders)
+ORDER BY 1;
