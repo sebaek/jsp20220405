7030fd1f5bf7eff5e3b810d90495a4344eda5f16
sebaek <sebaek@gmail.com>
2022-05-03 11:22:19 +0900

    outer join

--- WebContent/WEB-INF/sql/36JOIN.sql
+++ WebContent/WEB-INF/sql/36JOIN.sql
@@ -18,11 +18,23 @@ ORDER BY 1
 SELECT * FROM Board b INNER JOIN Reply r ON b.id = r.board_id;
 -- LEFT OUTER JOIN
 SELECT * FROM Board b LEFT OUTER JOIN Reply r ON b.id = r.board_id;
+SELECT * FROM Board b LEFT JOIN Reply r ON b.id = r.board_id;
 -- RIGHT OUTER JOIN
 SELECT * FROM Board b RIGHT OUTER JOIN Reply r ON b.id = r.board_id;
 SELECT * FROM Reply r RIGHT OUTER JOIN Board b ON b.id = r.board_id;
+SELECT * FROM Board b RIGHT JOIN Reply r ON b.id = r.board_id;
+SELECT * FROM Reply r RIGHT JOIN Board b ON b.id = r.board_id;
 
-
-
+-- 댓글이 없는 게시물 조회
+SELECT * 
+FROM Board b LEFT JOIN Reply r 
+ON b.id = r.board_id
+WHERE r.id IS NULL
+;
+-- 전체 게시물 조회 with 댓글 수
+SELECT b.*, COUNT(r.id) NumOfReply 
+FROM Board b LEFT JOIN Reply r ON b.id = r.board_id
+GROUP BY b.id
+ORDER BY b.id DESC;
 
 
